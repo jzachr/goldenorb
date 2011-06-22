c607e71a723e2202999049c8179578968ef881fb
Long Cao <long@raveldata.com>
2011-06-22 23:07:38 +0000

    Implemented InboundMessageQueue to create bins on demand

--- src/main/java/org/goldenorb/queue/InboundMessageQueue.java
+++ src/main/java/org/goldenorb/queue/InboundMessageQueue.java
@@ -1,7 +1,6 @@
 package org.goldenorb.queue;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -16,45 +15,56 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class InboundMessageQueue {
-	Map<String, List<Message<? extends Writable>>> inboundMessageMap = new HashMap<String, List<Message<? extends Writable>>>();
-	Set<String> verticesWithMessages = new HashSet<String>();
-	private Logger logger;
-	
-	public Map<String, List<Message<? extends Writable>>> getInboundMessageMap() {
-		return inboundMessageMap;
-	}
-
-	public Set<String> getVerticesWithMessages() {
-		return verticesWithMessages;
-	}
-
-	public InboundMessageQueue(Collection<String> ids){
-		logger = LoggerFactory.getLogger(InboundMessageQueue.class);
-		for(String id: ids){
-			inboundMessageMap.put(id, Collections.synchronizedList(new ArrayList<Message<? extends Writable>>()));
-		}
-	}
-	
-	public void addMessages(Messages ms){
-		for(Message<? extends Writable> m: ms.getList()){
-			addMessage(m);
-		}
-	}
-	
-	public void addMessage(Message<? extends Writable> m){	
-		inboundMessageMap.get(m.getDestinationVertex()).add(m);
-		synchronized(verticesWithMessages){
-			verticesWithMessages.add(m.getDestinationVertex());
-		}
-	}
-	
-	public void addNewVertex(String id){
-		synchronized(inboundMessageMap){
-			inboundMessageMap.put(id, Collections.synchronizedList(new ArrayList<Message<? extends Writable>>()));
-		}
-	}
-	
-	public List<Message<? extends Writable>> getMessage(String vertexID){
-		return inboundMessageMap.get(vertexID);
-	}
+  
+  Map<String,List<Message<? extends Writable>>> inboundMessageMap = new HashMap<String,List<Message<? extends Writable>>>();
+  Set<String> verticesWithMessages = new HashSet<String>();
+  private Logger logger;
+  
+  /**
+   * Constructs the InboundMessageQueue.
+   * 
+   * @param ids
+   */
+  public InboundMessageQueue() {
+    logger = LoggerFactory.getLogger(InboundMessageQueue.class);
+  }
+  
+  public void addMessages(Messages ms) {
+    for (Message<? extends Writable> m : ms.getList()) {
+      addMessage(m);
+    }
+  }
+  
+  public void addMessage(Message<? extends Writable> m) {
+    synchronized (inboundMessageMap) {
+      // creates an empty, synchronized ArrayList for a key (a Vertex) if the key doesn't already exist
+      if (inboundMessageMap.containsKey(m.getDestinationVertex()) == false) {
+        inboundMessageMap.put(m.getDestinationVertex(),
+          Collections.synchronizedList(new ArrayList<Message<? extends Writable>>()));
+      }
+      
+      inboundMessageMap.get(m.getDestinationVertex()).add(m);
+      synchronized (verticesWithMessages) {
+        verticesWithMessages.add(m.getDestinationVertex());
+      }
+    }
+  }
+  
+  public void addNewVertex(String id) {
+    synchronized (inboundMessageMap) {
+      inboundMessageMap.put(id, Collections.synchronizedList(new ArrayList<Message<? extends Writable>>()));
+    }
+  }
+  
+  public List<Message<? extends Writable>> getMessage(String vertexID) {
+    return inboundMessageMap.get(vertexID);
+  }
+  
+  public Map<String,List<Message<? extends Writable>>> getInboundMessageMap() {
+    return inboundMessageMap;
+  }
+  
+  public Set<String> getVerticesWithMessages() {
+    return verticesWithMessages;
+  }
 }
