ccb544d5d40bf7a1b3022691f08966ebcadbe78c
Long Cao <long@raveldata.com>
2011-06-24 22:34:23 +0000

    Committing QueueInfoCollector for testing purposes.

--- src/test/java/org/goldenorb/queue/QueueInfoCollector.java
+++ src/test/java/org/goldenorb/queue/QueueInfoCollector.java
@@ -1,33 +1,31 @@
 package org.goldenorb.queue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.goldenorb.Message;
 import org.goldenorb.Messages;
 import org.goldenorb.OrbPartitionCommunicationProtocol;
+import org.goldenorb.Vertex;
 import org.goldenorb.Vertices;
 
 public class QueueInfoCollector implements OrbPartitionCommunicationProtocol {
   
-  List<Message> list;
+  List<Message> mList = Collections.synchronizedList(new ArrayList<Message>());
+  List<Vertex> vList = Collections.synchronizedList(new ArrayList<Vertex>());
   
   @Override
   public void sendMessages(Messages messages) {
-    //list = messages.getList();
-    //System.out.println("I got called.");
-    //System.out.println(messages.toString());
-  }
-  
-  public List<Message> getMessageList() {
-    return list;
+    // add all outgoing Messages to a synchronizedList to check if any Messages are lost
+    mList.addAll(messages.getList());
   }
   
   @Override
   public void sendVertices(Vertices vertices) {
-    
+ // add all outgoing Vertices to a synchronizedList to check if any messages are lost
+    vList.addAll(vertices.getArrayList());
   }
   
   @Override
