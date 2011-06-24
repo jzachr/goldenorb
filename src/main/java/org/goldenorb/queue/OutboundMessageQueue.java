5dd7f4fed5cb6b84ee2f6e55dcfa89a5920824b7
Long Cao <long@raveldata.com>
2011-06-24 22:31:56 +0000

    Committed an updated OutboundMessageQueue and its test.

--- src/main/java/org/goldenorb/queue/OutboundMessageQueue.java
+++ src/main/java/org/goldenorb/queue/OutboundMessageQueue.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.hadoop.io.Writable;
 import org.goldenorb.Message;
@@ -17,29 +20,33 @@ import org.slf4j.LoggerFactory;
 public class OutboundMessageQueue {
   private Logger omqLogger;
   
-  List<Map<String,List<Message<? extends Writable>>>> partitionMessageMapsList;
   private int numberOfPartitions;
-  private List<Integer> partitionMessageCounter;
   private int maxMessages;
   public Map<Integer,OrbPartitionCommunicationProtocol> orbClients;
-  private Class<? extends Message<? extends Writable>> messageType;
+  private Class<? extends Message<? extends Writable>> messageClass;
   private int partitionId;
+  
+  PartitionMessagingObject pmo;
   
   public OutboundMessageQueue(int numberOfPartitions,
                               int maxMessages,
                               Map<Integer,OrbPartitionCommunicationProtocol> orbClients,
-                              Class<? extends Message<? extends Writable>> messageType,
+                              Class<? extends Message<? extends Writable>> messageClass,
                               int partitionId) {
     omqLogger = LoggerFactory.getLogger(OutboundMessageQueue.class);
-    
-    this.partitionId = partitionId;
+
     this.numberOfPartitions = numberOfPartitions;
     this.maxMessages = maxMessages;
     this.orbClients = orbClients;
-    this.messageType = messageType;
+    this.messageClass = messageClass;
+    this.partitionId = partitionId;
+    
+    List<Map<String,List<Message<? extends Writable>>>> partitionMessageMapsList;
+    List<Integer> partitionMessageCounter;
     
     // creates a HashMap<Vertex,Message List> for each outbound partition
-    partitionMessageMapsList = new ArrayList<Map<String,List<Message<? extends Writable>>>>(numberOfPartitions);
+    partitionMessageMapsList = new ArrayList<Map<String,List<Message<? extends Writable>>>>(
+        numberOfPartitions);
     for (int i = 0; i < numberOfPartitions; i++) {
       partitionMessageMapsList.add(Collections
           .synchronizedMap(new HashMap<String,List<Message<? extends Writable>>>()));
@@ -50,53 +57,59 @@ public class OutboundMessageQueue {
     for (int i = 0; i < numberOfPartitions; i++) {
       partitionMessageCounter.add(0);
     }
+    
+    this.pmo = new PartitionMessagingObject(partitionMessageMapsList, partitionMessageCounter);
   }
   
   public void sendMessage(Message<? extends Writable> m) {
-    
-    // get the HashMap bin that is unique to the DestinationVertex
-    int messageHash = Math.abs(m.getDestinationVertex().hashCode()) % numberOfPartitions;
-    Map<String,List<Message<? extends Writable>>> currentPartition = partitionMessageMapsList.get(messageHash);
-    Integer messageCounter;
-    synchronized (partitionMessageCounter) {
-      synchronized (currentPartition) {
-        messageCounter = partitionMessageCounter.get(messageHash);
-        messageCounter++; // increment the message counter
-        partitionMessageCounter.set(messageHash, messageCounter);
-      }
+    synchronized (pmo) {
+      // get the HashMap bin that is unique to the DestinationVertex
+      int messageHash = Math.abs(m.getDestinationVertex().hashCode()) % numberOfPartitions;
+      Map<String,List<Message<? extends Writable>>> currentPartition = pmo.partitionMessageMapsList.get(messageHash);
       
-      // if Vertex exists as a key, add the Message
-      // else create a new synchronized list for the key on demand, then put the list in the map
-      if (currentPartition.containsKey(m.getDestinationVertex())) {
-        currentPartition.get(m.getDestinationVertex()).add(m);
-      } else {
-        List<Message<? extends Writable>> messageList = Collections
-            .synchronizedList(new ArrayList<Message<? extends Writable>>());
-        messageList.add(m);
-        currentPartition.put(m.getDestinationVertex(), messageList);
-      }
-      
-      // once the expected number of messages is met, begins the message sending operation
-      if (messageCounter >= maxMessages) {
-        Messages messagesToBeSent = new Messages(messageType);
-        
-        // collects the messages associated to each key and adds them to a Messages object to be sent
-        for (Collection<Message<? extends Writable>> ms : currentPartition.values()) {
-          for (Message<? extends Writable> message : ms) {
-            messagesToBeSent.add(message);
-          }
+      Integer messageCounter;
+      synchronized (pmo.partitionMessageCounter) {
+        synchronized (currentPartition) {
+          messageCounter = pmo.partitionMessageCounter.get(messageHash);
+          messageCounter++; // increment the message counter
+          pmo.partitionMessageCounter.set(messageHash, messageCounter);
         }
         
-        // logger stuff
-        omqLogger.info(this.toString() + " Partition: " + Integer.toString(partitionId)
-                       + "Sending bulk messages. Count: " + messageCounter + ", " + messagesToBeSent.size());
-        omqLogger.info(messageType.getName());
+        // if Vertex exists as a key, add the Message
+        // else create a new synchronized list for the key on demand, then put the list in the map
+        if (currentPartition.containsKey(m.getDestinationVertex())) {
+          currentPartition.get(m.getDestinationVertex()).add(m);
+        } else {
+          List<Message<? extends Writable>> messageList = Collections
+              .synchronizedList(new ArrayList<Message<? extends Writable>>());
+          messageList.add(m);
+          currentPartition.put(m.getDestinationVertex(), messageList);
+        }
         
-        // sends the Messages to the partition as specified over RPC, then creates a fresh, empty Map in its place
-        orbClients.get(messageHash).sendMessages(messagesToBeSent);
-        partitionMessageMapsList.set(messageHash,
-          Collections.synchronizedMap(new HashMap<String,List<Message<? extends Writable>>>()));
-        partitionMessageCounter.set(messageHash, new Integer(0)); // reset counter to 0
+        // once the expected number of messages is met, begins the message sending operation
+        if (messageCounter >= maxMessages) {
+          Messages messagesToBeSent = new Messages(messageClass);
+          
+          // collects the messages associated to each key and adds them to a Messages object to be sent
+          for (Collection<Message<? extends Writable>> ms : currentPartition.values()) {
+            for (Message<? extends Writable> message : ms) {
+              messagesToBeSent.add(message);
+            }
+          }
+          
+          // logger stuff
+          omqLogger
+              .info(this.toString() + " Partition: " + Integer.toString(partitionId)
+                    + "Sending bulk messages. Count: " + messageCounter + ", " + messagesToBeSent.size());
+          omqLogger.info(messageClass.getName());
+          
+          // sends the Messages to the partition as specified over RPC, then creates a fresh, empty Map in its
+          // place
+          orbClients.get(messageHash).sendMessages(messagesToBeSent);
+          pmo.partitionMessageMapsList.set(messageHash,
+            Collections.synchronizedMap(new HashMap<String,List<Message<? extends Writable>>>()));
+          pmo.partitionMessageCounter.set(messageHash, new Integer(0)); // reset counter to 0
+        }
       }
     }
   }
@@ -104,16 +117,38 @@ public class OutboundMessageQueue {
   public void sendRemainingMessages() {
     
     for (int partitionID = 0; partitionID < numberOfPartitions; partitionID++) {
-      Messages messagesToBeSent = new Messages(messageType);
+      Messages messagesToBeSent = new Messages(messageClass);
       
-      for (Collection<Message<? extends Writable>> ms : partitionMessageMapsList.get(partitionID).values()) {
+      for (Collection<Message<? extends Writable>> ms : pmo.partitionMessageMapsList.get(partitionID)
+          .values()) {
         for (Message<? extends Writable> message : ms) {
           messagesToBeSent.add(message);
         }
       }
-      omqLogger.info("Sending bulk messages.  Count: " + partitionMessageCounter.get(partitionID) + ", "
+      omqLogger.info("Sending bulk messages.  Count: " + pmo.partitionMessageCounter.get(partitionID) + ", "
                      + messagesToBeSent.size());
       orbClients.get(partitionID).sendMessages(messagesToBeSent);
     }
+  }
+  
+  class PartitionMessagingObject {
+    
+    List<Map<String,List<Message<? extends Writable>>>> partitionMessageMapsList;
+    List<Integer> partitionMessageCounter;
+    
+    public PartitionMessagingObject(List<Map<String,List<Message<? extends Writable>>>> partitionMessageMapsList,
+                                    List<Integer> partitionMessageCounter) {
+      this.partitionMessageMapsList = partitionMessageMapsList;
+      this.partitionMessageCounter = partitionMessageCounter;
+    }
+    
+    public List<Map<String,List<Message<? extends Writable>>>> getMapsList() {
+      return partitionMessageMapsList;
+    }
+    
+    public List<Integer> getMessageCounter() {
+      return partitionMessageCounter;
+    }
+    
   }
 }
--- src/test/java/org/goldenorb/queue/OutboundMessageQueueTest.java
+++ src/test/java/org/goldenorb/queue/OutboundMessageQueueTest.java
@@ -2,16 +2,16 @@ package org.goldenorb.queue;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.goldenorb.Message;
+import org.goldenorb.Messages;
 import org.goldenorb.OrbPartitionCommunicationProtocol;
 import org.goldenorb.types.message.TextMessage;
 import org.junit.Test;
@@ -22,36 +22,90 @@ public class OutboundMessageQueueTest {
   
   @Test
   public void testOutBoundMessageQueue() throws Exception {
-    
     // OutboundMessageQueue settings
-    int numberOfPartitions = 5;
-    int maxMessages = 5000;
+    int numberOfPartitions = 100;
+    int numOfMessagesToSendPerThread = 10000; // max number of Messages to be sent by a Thread
+    int numOfMessagesPerBlock = 1000; // max number of Messages to trigger a send operation by the queue
     int partitionId = 1;
-    Class<? extends Message<? extends Writable>> messageType = TextMessage.class;
+    Class<? extends Message<? extends Writable>> messageClass = TextMessage.class;
     Map<Integer,OrbPartitionCommunicationProtocol> orbClients = new HashMap<Integer,OrbPartitionCommunicationProtocol>();
-    // make the orbClients thing
     for (int i = 0; i < numberOfPartitions; i++) {
       orbClients.put(new Integer(i), infoCollector);
     }
     
-    OutboundMessageQueue omq = new OutboundMessageQueue(numberOfPartitions, maxMessages, orbClients,
-        messageType, partitionId);
+    OutboundMessageQueue omq = new OutboundMessageQueue(numberOfPartitions, numOfMessagesPerBlock,
+        orbClients, messageClass, partitionId);
     
-    int destVertex = 0;
-    TextMessage m = new TextMessage(Integer.toString(destVertex), new Text("TEST MESSAGE LOL"));
-    
-    omq.sendMessage(m);
-    
-    /*Iterator iter = omq.orbClients.keySet().iterator();
-    while (iter.hasNext()) {
-      System.out.println(iter.next());
+    // initialize the Threads and pass them their test Messages
+    CountDownLatch startLatch = new CountDownLatch(1);
+    CountDownLatch everyoneDoneLatch = new CountDownLatch(numberOfPartitions);
+    for (int i = 0; i < numberOfPartitions; i++) {
+      Messages msgs = new Messages(TextMessage.class);
+      for (int p = 0; p < numOfMessagesToSendPerThread; p++) {
+        TextMessage txtmsg = new TextMessage(Integer.toString(i), new Text("test message "
+                                                                           + Integer.toString(p)));
+        msgs.add(txtmsg);
+      }
+      
+      OutboundMessageThread obmThread = new OutboundMessageThread(msgs, omq, startLatch, everyoneDoneLatch);
+      obmThread.start(); // initialize a Thread
     }
     
-    Iterator iter1 = omq.orbClients.entrySet().iterator();
-    while (iter1.hasNext()) {
-      System.out.println(iter1.next());
-    } */
+    startLatch.countDown(); // start all Threads simultaneously
+    
+    everyoneDoneLatch.await(); // wait until all Threads are done
     
     assertThat(omq, notNullValue());
+    assertTrue(infoCollector.mList.size() == (numberOfPartitions * numOfMessagesToSendPerThread));
+  }
+}
+
+/**
+ * This class defines the Threads that can be used to add messages to an OutboundMessageQueue simultaneously.
+ * 
+ * @author long
+ * 
+ */
+class OutboundMessageThread extends Thread {
+  
+  private Messages msgs;
+  private OutboundMessageQueue omq;
+  private CountDownLatch startLatch;
+  private CountDownLatch everyoneDoneLatch;
+  
+  /**
+   * Constructs an OutboundMessageThread.
+   * 
+   * @param msgs
+   * @param omq
+   * @param startLatch
+   * @param everyoneDoneLatch
+   */
+  public OutboundMessageThread(Messages msgs,
+                               OutboundMessageQueue omq,
+                               CountDownLatch startLatch,
+                               CountDownLatch everyoneDoneLatch) {
+    this.msgs = msgs;
+    this.omq = omq;
+    this.startLatch = startLatch;
+    this.everyoneDoneLatch = everyoneDoneLatch;
+  }
+  
+  /**
+   * Adds messages to the OutboundMessageQueue.
+   */
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  public void run() {
+    try {
+      startLatch.await();
+      for (Message msg : msgs.getList()) {
+        omq.sendMessage(msg);
+      }
+      everyoneDoneLatch.countDown();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+    }
   }
 }
