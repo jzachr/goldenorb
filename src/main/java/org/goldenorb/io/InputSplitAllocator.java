22ed13c2beb8693b9da32e7f3a935b5da0d89152
Zach Richardson <zach@raveldata.com>
2011-06-26 17:31:54 +0000

    InputSplit assignment is complete

--- src/main/java/org/goldenorb/io/InputSplitAllocator.java
+++ src/main/java/org/goldenorb/io/InputSplitAllocator.java
@@ -18,7 +18,8 @@
 
 package org.goldenorb.io;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +28,6 @@ import org.goldenorb.conf.OrbConfigurable;
 import org.goldenorb.conf.OrbConfiguration;
 import org.goldenorb.io.input.RawSplit;
 import org.goldenorb.jet.OrbPartitionMember;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,76 +36,119 @@ public class InputSplitAllocator implements OrbConfigurable {
   private OrbConfiguration orbConf;
   private List<OrbPartitionMember> orbPartitionMembers;
   
-  private Map<String, Map<Integer, List<RawSplit>>> hostToPortToRawSplits = new HashMap<String, Map<Integer, List<RawSplit>>>();
-  private Map<String, Integer> hostToRawSplitCount = new HashMap<String, Integer>();
+  private Map<String,Map<Integer,List<RawSplit>>> hostToPortToRawSplits = new HashMap<String,Map<Integer,List<RawSplit>>>();
+  private Map<String,Integer> hostToRawSplitCount = new HashMap<String,Integer>();
   
-  private Logger logger;
+  private Logger LOG;
   
   public InputSplitAllocator(OrbConfiguration orbConf, List<OrbPartitionMember> orbPartitionMembers) {
     this.orbConf = orbConf;
     this.orbPartitionMembers = orbPartitionMembers;
-    logger = LoggerFactory.getLogger(InputSplitAllocator.class);
+    LOG = LoggerFactory.getLogger(InputSplitAllocator.class);
+    
+    // initialize maps for placing rawSplits
+    for (OrbPartitionMember orbPartitionMember : orbPartitionMembers) {
+      if (!hostToRawSplitCount.containsKey(orbPartitionMember.getHostname())) {
+        hostToRawSplitCount.put(orbPartitionMember.getHostname(), 0);
+      }
+      if (!hostToPortToRawSplits.containsKey(orbPartitionMember.getHostname())) {
+        hostToPortToRawSplits.put(orbPartitionMember.getHostname(), new HashMap<Integer,List<RawSplit>>());
+      }
+      Map<Integer,List<RawSplit>> portMap = hostToPortToRawSplits.get(orbPartitionMember.getHostname());
+      if (!portMap.containsKey(orbPartitionMember.getPort())) {
+        portMap.put(orbPartitionMember.getPort(), new ArrayList<RawSplit>());
+      }
+    }
   }
   
-  public Map<OrbPartitionMember,List<RawSplit>> assignInputSplits(List<RawSplit> rawSplits) {
-    Map<OrbPartitionMember,List<RawSplit>> mapped = new HashMap<OrbPartitionMember,List<RawSplit>>();
+  public Map<OrbPartitionMember,List<RawSplit>> assignInputSplits(Collection<RawSplit> rawSplits) {
+    Map<OrbPartitionMember,List<RawSplit>> mapOfSplitsToPartitions = new HashMap<OrbPartitionMember,List<RawSplit>>();
+    List<RawSplit> notLocalRawSplits = new ArrayList<RawSplit>();
     
-    //creates a new HashMap<PortNumber, List<RawSplit>> for every host location if it doesn't exist
-    for (RawSplit rSplit : rawSplits) {
-      for (String rSplitLocation : rSplit.getLocations()) {
-        if (!hostToPortToRawSplits.containsKey(rSplitLocation)) {
-          hostToPortToRawSplits.put(rSplitLocation, new HashMap<Integer, List<RawSplit>>());
-        }
+    for (RawSplit rawSplit : rawSplits) {
+      List<String> viableLocations = getViableHosts(rawSplit.getLocations());
+      if (viableLocations.size() < 1) {
+        notLocalRawSplits.add(rawSplit);
+        LOG.debug("Non-local split found: " + rawSplit);
+      } else {
+        String host = getLightestHost(viableLocations);
+        int port = getLightestPort(host);
+        List<RawSplit> rawSplitAssignedList = hostToPortToRawSplits.get(host).get(port);
+        rawSplitAssignedList.add(rawSplit);
+        hostToPortToRawSplits.get(host).put(port, rawSplitAssignedList);
+        int count = hostToRawSplitCount.get(host);
+        count++;
+        hostToRawSplitCount.put(host, count);
       }
     }
     
-    for (RawSplit rSplit : rawSplits) {
-      try {
-        System.out.println(getLightestHost(rSplit));
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+    for (RawSplit rawSplit : notLocalRawSplits) {
+      String host = getLightestHostAll();
+      int port = getLightestPort(host);
+      List<RawSplit> rawSplitAssignedList = hostToPortToRawSplits.get(host).get(port);
+      rawSplitAssignedList.add(rawSplit);
+      hostToPortToRawSplits.get(host).put(port, rawSplitAssignedList);
+      int count = hostToRawSplitCount.get(host);
+      count++;
+      hostToRawSplitCount.put(host, count);
     }
     
-    /*
-    String lightestHost = getLightestHost(rawSplit);
-    int lightestPort = getLightestPort(lightestHost);
-    List<RawSplit> rawSplits = hostToPortToRawSplits.get(lightestHost).get(lightestPort);
-    rawSplits.add(rawSplit);
-    hostToPortToRawSplits.get(lightestHost).put(lightestPort, rawSplits);
-    */
+    for (OrbPartitionMember orbPartitionMember : orbPartitionMembers) {
+      mapOfSplitsToPartitions.put(orbPartitionMember,
+        hostToPortToRawSplits.get(orbPartitionMember.getHostname()).get(orbPartitionMember.getPort()));
+    }
     
-    return mapped;
+    return mapOfSplitsToPartitions;
   }
   
-  public String getLightestHost(RawSplit rawSplit) throws IOException {
-    String smallestHost = "";
-    int smallestInt = Integer.MAX_VALUE;
-    
-    for (String location : rawSplit.getLocations()) {
-      logger.info("RawSplit returned locations: " + location);
-      int locationCount = hostToRawSplitCount.get(location);
-      if (locationCount < smallestInt) {
-        smallestInt = locationCount;
-        smallestHost = location;
+  private List<String> getViableHosts(String[] hosts) {
+    List<String> viableHosts = new ArrayList<String>();
+    for (String host : hosts) {
+      if (hostToRawSplitCount.containsKey(host)) {
+        viableHosts.add(host);
       }
     }
-    return smallestHost;
+    return viableHosts;
   }
   
-  public int getLightestPort(String host) throws IOException {
-    int smallestPort = 0;
-    int smallestInt = Integer.MAX_VALUE;
-    
-    for (int port : hostToPortToRawSplits.get(host).keySet()) {
-      logger.info("Available ports: " + port);
-      int portCount = hostToPortToRawSplits.get(host).get(port).size();
-      if (portCount < smallestInt) {
-        smallestInt = portCount;
-        smallestPort = port;
+  private String getLightestHost(List<String> hosts) {
+    String lightestHost = null;
+    int lightestCount = Integer.MAX_VALUE;
+    for (String host : hosts) {
+      int hostCount = hostToRawSplitCount.get(host);
+      if (hostCount < lightestCount) {
+        lightestCount = hostCount;
+        lightestHost = host;
       }
     }
-    return smallestPort;
+    return lightestHost;
+  }
+  
+  private int getLightestPort(String host) {
+    int lightestPort = 0;
+    int lightestCount = Integer.MAX_VALUE;
+    Map<Integer,List<RawSplit>> portMap = hostToPortToRawSplits.get(host);
+    for (int port : portMap.keySet()) {
+      int portCount = portMap.get(port).size();
+      if (portCount < lightestCount) {
+        lightestCount = portCount;
+        lightestPort = port;
+      }
+    }
+    return lightestPort;
+  }
+  
+  private String getLightestHostAll() {
+    String lightestHost = null;
+    int lightestCount = Integer.MAX_VALUE;
+    for (String host : hostToRawSplitCount.keySet()) {
+      int hostCount = hostToRawSplitCount.get(host);
+      if (hostCount < lightestCount) {
+        lightestCount = hostCount;
+        lightestHost = host;
+      }
+    }
+    return lightestHost;
   }
   
   @Override
--- src/main/java/org/goldenorb/io/input/RawSplit.java
+++ src/main/java/org/goldenorb/io/input/RawSplit.java
@@ -70,5 +70,4 @@ public class RawSplit implements Writable {
     public void setDataLength(long l) {
       dataLength = l;
     }
-    
   }
