f0bc40b807dff9114bad60f1b2d393e01727c2da
Sandeep Parikh <sandeep@raveldata.com>
2011-07-15 16:54:13 +0000

    changed the params for shortest path graph generator

--- contrib/src/test/python/randomShortestPath.py
+++ contrib/src/test/python/randomShortestPath.py
@@ -1,12 +1,15 @@
 import random
 
+nodes = 100000
+edges_per_node = 100
+
 def main():
-    for i in range(1000):
+    for i in range(nodes):
         out = ''
-        for j in range(10):
-            v = random.randint(0,999)
+        for j in range(edges_per_node):
+            v = random.randint(0,nodes-1)
             while v is j:
-                v = random.randint(0,999)
+                v = random.randint(0,nodes-1)
             w = random.randint(10,20)
             out += "\t%d:%d" % (v,w)
         print "%d%s" % (i,out)
