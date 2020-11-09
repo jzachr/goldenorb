d0195495ac12ba2e4cd702e56e036f30542d6a8b
1shekhar <42611113+1shekhar@users.noreply.github.com>
2020-11-09 10:19:22 +0530

    Create Fibonacci.java

---
+++ Java/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int count = 7, num1 = 0, num2 = 1;
+        System.out.print("Fibonacci Series of "+count+" numbers:");
+
+        for (int i = 1; i <= count; ++i)
+        {
+            System.out.print(num1+" ");
+            int sumOfPrevTwo = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrevTwo;
+        }
+    }
+}
